f6db307b0220da6315b827290b83a684fe87045d
0feeling <timothee.meunier@gmail.com>
2024-11-13 21:49:20 +0100

    AddThoughtForm.js

--- src/AddThoughtForm.js
+++ src/AddThoughtForm.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { generateId, getNewExpirationTime } from './utils/Utilities';
 
-export function AddThoughtForm(props) {
+export function AddThoughtForm({ addThought }) {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newThought = {
+      id: generateId(),
+      text: text,
+      expiresAt: getNewExpirationTime(),
+      isSaved: false,
+    };
+
+    addThought(newThought);
+    setText('');
+  };
+
   return (
-    <form className="AddThoughtForm">
+    <form className="AddThoughtForm" onSubmit={handleSubmit}>
       <input
         type="text"
         aria-label="What's on your mind?"
-        placeholder="What's on your mind?"
+        placeholder="Libérez votre Esprit. Ici, écrivez vos Pensées"
+        value={text}
+        onChange={handleTextChange}
       />
-      <input type="submit" value="Add" />
+      <input type="submit" value="Ajoutez" />
     </form>
   );
 }
